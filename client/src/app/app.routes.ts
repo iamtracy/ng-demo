c547989c7150f59d44270a4b83176fa186476d75
Tracy Lee <tleecoding@gmail.com>
2025-05-25 09:25:52 -0400

    auth

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'
+import { AboutComponent } from './about/about.component';
 import { canActivateAuthRole } from './core/auth/guards/auth.guard'
 
 export const routes: Routes = [
@@ -9,6 +10,12 @@ export const routes: Routes = [
         canActivate: [canActivateAuthRole],
         data: {
             roles: ['user']
-        }
+        },
+        children: [
+            {
+                path: 'about',
+                component: AboutComponent,
+            }
+        ]
     }
 ]
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,22 +1,45 @@
 import { Component, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HomeService } from './home.service'
-import { HttpClient } from '@angular/common/http'
+import { MatTableDataSource, MatTableModule } from '@angular/material/table'
+import { Observable, of, tap } from 'rxjs'
+import { MatButtonModule } from '@angular/material/button'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-   hello home
-  `,
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'message', 'createdAt', 'updatedAt']
+  dataSource = new MatTableDataSource<any>([])
+  greetings$: Observable<any[]> = of([])
+
+  form = new FormGroup({
+    message: new FormControl('')
+  })
 
   constructor(private homeService: HomeService) {}
 
   ngOnInit(): void {
-    console.log('ngOnInit')
     this.homeService.getGreetings().subscribe()
+    this.greetings$ = this.homeService.greetings$.pipe(
+      tap((greetings) => this.dataSource.data = greetings)
+    )
+  }
+
+  onSubmit() {
+    this.homeService.createGreeting(this.form.value.message as string).subscribe()
   }
 }
